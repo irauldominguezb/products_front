18e87e812fd7f5a6cd66de9656201da56343a923
irauldominguezb <irauldominguezb@users.noreply.github.com>
2024-08-05 04:23:21 +0000

    Commit the artifacts

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3081332716"
+    serviceWorkerVersion: "1538044634"
   }
 });
